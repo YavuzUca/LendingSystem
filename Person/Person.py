ff995f95519f19dddba73a406f96112c6aff8970
Yavuz Uca <yavuz_uca@hotmail.com>
2021-04-21 22:35:16 +0200

    Medium shit

--- Person/Person.py
+++ Person/Person.py
@@ -1,8 +1,8 @@
 class Person:
     def __init__(self, id, gender, nameSet, firstName, surname, permissionLevel = 1):
-        self._id = id
-        self._gender = gender
-        self._nameSet = nameSet
-        self._firstName = firstName
-        self._surname = surname
-        self._permissionLevel = permissionLevel
+        self.id = id
+        self.gender = gender
+        self.nameSet = nameSet
+        self.firstName = firstName
+        self.surname = surname
+        self.permissionLevel = permissionLevel
--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from Person.Subscriber import Subscriber
 from FrontEnd import Homepage
 
 # loanAdministration = LoanAdministration()
-# horror = Catalog("Horror")
-# guy = Book("Guy", "Corne", "1111111111X", "The Netherlands", "Dutch", "bol.com", "bol.com/777.png", 107, 2001)
+horror = Catalog("Horror")
+guy = Book("Guy", "Corne", "1111111111X", "The Netherlands", "Dutch", "bol.com", "bol.com/777.png", 107, 2001)
+guy_copy = Bookitem(guy)
 # horror.addBook(guy)
 # guyItemOne = Bookitem(guy)
 # guyItemTwo = Bookitem(guy)
@@ -30,6 +31,19 @@ corne = Subscriber(1, "Male", "Dutch", "Corne", "den Breejen", "bogerd 9", "2922
 #     for j in i.list_book:
 #         print(j)
 
-run = Page()
-run.homePage()
+# run = Page()
+# run.homePage()
+system = LoanAdministration()
+loanitem = Loanitem(corne)
+loanitem.addBookToList(guy_copy)
 
+# system.update(loanitem)
+# system.createBackup()
+# print(system.borrowedBooks)
+system2 = LoanAdministration()
+system2.restoreBackup()
+print(system2.borrowedBooks)
+print(guy_copy.available)
+for i in system.borrowedBooks:
+    print(i.id)
+    print(i.list_bookitems[0].book_obj.title)
