cadb0b2069b6b751c44d65e0341af7fe586fb352
Yavuz Uca <yavuz_uca@hotmail.com>
2021-04-24 16:49:18 +0200

    New version

--- FrontEnd/Homepage.py
+++ FrontEnd/Homepage.py
@@ -11,6 +11,7 @@ from Library.Catalog import Catalog
 class Page:
     def __init__(self):
         self.currently_loggedin = None
+        self.currently_loggedin_perm = None
         self.usersystem = UserAdministration()
         self.loansystem = LoanAdministration()
         self.listsAllCat = []
@@ -81,17 +82,18 @@ class Page:
             self.homePage()
 
     def registerUser(self):
-
         gender_input = input("Enter your gender (Male/Female: ")
         name_set_input = input("Enter your nationalty: ")
         username_input = input("Enter your username: ")
+        while (not self.username_check(username_input)):
+            username_input = input("Enter your username: ")
         firstName_input = input("Enter your first name: ")
         surname_input = input("Enter your surname: ")
         address_input = input("Enter your home address (street name + housenumber): ")
         zipcode_input = input("Enter your zipcode (Format: 0000AA): ")
         city_input = input("Enter your city: ")
         emailAddress_input = input("Enter your emailaddress: ")
-        phonenumber_input = input("Enter your phone number: ")
+        phonenumber_input = str(input("Enter your phone number: "))
 
         newSubscriber = Subscriber(gender_input, name_set_input, firstName_input, surname_input, address_input,
                                    zipcode_input, city_input, emailAddress_input, username_input, phonenumber_input)
@@ -100,12 +102,20 @@ class Page:
         print("User " + username_input + " has been created")
         return self.homePage()
 
+    def username_check(self, username_input):
+        for user in self.usersystem.personlist:
+            if username_input == user.username:
+                print("Usernamer already in use, please type another.")
+                return False
+        return True
+
     def logIn(self):
         print("Please enter your username:")
         user_input = input()
         for i in self.usersystem.personlist:
             if i.username == user_input:
                 self.currently_loggedin = i.username
+                self.currently_loggedin_perm = i.permissionLevel
                 if i.permissionLevel == 0:
                     return self.admin_page()
                 else:
@@ -131,14 +141,14 @@ class Page:
             year = 0
 
         books = []
-        for cat in self.list_cat:
+        for cat in self.listsAllCat:
             tmpBooks = cat.searchBook(title, author, country, language, pages, year)
             if tmpBooks:
                 books.append(tmpBooks)
 
         if not books:
             print("\nCould not find a book by the given criteria.\n")
-            return self.homePage() if self.currently_loggedin is None else self.user_page()
+            return self.homePage() if self.currently_loggedin is None else self.user_page() if self.currently_loggedin_perm == 1 else self.admin_page()
 
         for catalog in books:
             for book in catalog:
@@ -150,9 +160,7 @@ class Page:
                     print("In stock: Yes" + "\n")
                 else:
                     print("In stock: No" + "\n")
-                
-
-
+        return self.homePage() if self.currently_loggedin is None else self.user_page() if self.currently_loggedin_perm == 1 else self.admin_page()
 
     def user_page(self):
         print("1. Search Book")
@@ -172,6 +180,7 @@ class Page:
             self.rent_book()
         elif keypress == "5":
             self.currently_loggedin = None
+            self.currently_loggedin_perm = None
             self.homePage()
         else:
             print("Invalid keypress")
@@ -191,40 +200,35 @@ class Page:
                 print("Your emailaddress    = " + i.emailAddress)
                 print("Your phone number    = " + i.telephoneNumber)
 
-        return self.homePage()
+        return self.user_page()
 
     def myloans(self):
         for loanitem in self.loansystem.borrowedBooks:
             if self.currently_loggedin == loanitem.subscriber.username:
                 for book_item in loanitem.list_bookitems:
-                    if len(loanitem.list_bookitems) < 1:
-                        print("No books have been rented so far.")
-                        return self.user_page()
                     print(
                         f"Name: {book_item.book_obj.title}\nStartdate: {loanitem.startdate}\nEnddate: {loanitem.enddate}\n")
-                    return self.user_page()
-
+                # return self.user_page()
+        return self.user_page()
 
     def rent_book(self):
         sub = self.get_subobject()
         loan = Loanitem(sub)
-        addingbooks = 'y'
-        while addingbooks == 'y':
-            search_input = input("Add a book to your list by typing it's title: ")
-            for cat in self.listsAllCat:
-                if addingbooks == 'y':
-                    for book in cat.list_books:
-                        if search_input == book.title:
-                            for copy in book.list_book:
-                                if copy.checkAvailability():
-                                    loan.addBookToList(copy)
-                                    addingbooks = input(
-                                        "Book " + book.title + " is added to your list. Want to add another book? (y/n): ")
-
-                        else:
-                            addingbooks = input("Book not found. Wanna try again? (y/n)")
-        loan.borrowBook()
-        self.loansystem.update(loan)
+        search_input = input("Add a book to your list by typing it's title: ")
+        for cat in self.listsAllCat:
+            for book in cat.list_books:
+                if search_input == book.title:
+                    if not book.checkAvailibility():
+                        print("Book is out of stock.")
+                        return self.user_page()
+                    for copy in book.list_book:
+                        if copy.checkAvailability():
+                            loan.addBookToSingleList(copy)
+                            print("Book added.")
+                            loan.borrowBook()
+                            self.loansystem.update(loan)
+                            return self.user_page()
+        print("Error, book not found.")
         return self.user_page()
 
     def get_subobject(self):
@@ -256,6 +260,7 @@ class Page:
             self.makeBackup()
         elif keypress == "7":
             self.currently_loggedin = None
+            self.currently_loggedin_perm = None
             self.homePage()
         else:
             print("Invalid keypress\n")
@@ -298,7 +303,6 @@ class Page:
             if ans == "y":
                 print("Type name of the Category.\n")
                 namecat = input()
-                print(len(self.listsAllCat))
                 while not namecat in [i.genre for i in self.listsAllCat]:
                     print("Name category does not exist, try again.\n")
                     namecat = input()
@@ -306,6 +310,7 @@ class Page:
                     if i.genre == namecat:
                         book = Book(author, country, imageLink, language, link, pages, title, year)
                         i.addBook(book)
+                        print("Book has been added.")
                         return self.admin_page()
             else:
                 for i in self.listsAllCat:
@@ -352,13 +357,11 @@ class Page:
         for cat in self.listsAllCat:
             for book in cat.list_books:
                 if book.title == name:
+                    print(book)
                     bookitem = Bookitem(book)
                     book.update(bookitem)
                     print("Book copy added!")
                     return self.admin_page()
-                else:
-                    print("It appears that the booktitle you put does not exist.")
-                    return self.admin_page()
 
-
-
+        print("It appears that the booktitle you put does not exist.")
+        return self.admin_page()
